dfbee1a96b0f52353eab96822f50108ab3345e10
laurita-santos <73966384+laurita-santos@users.noreply.github.com>
2021-11-03 12:20:18 -0300

    Update ordinal_partition_network.m

--- ordinal_partition_network.m
+++ ordinal_partition_network.m
@@ -56,7 +56,7 @@ for n=1:length(numb)-1
 end
 
 % % permutation entropy:  it is estimated by counting the relative
-% % ocurrence of each symbol in the symbolic dynamics S - equation 3.1
+% % ocurrence of each symbol in the symbolic dynamics S
 par = unique(numb);
 n_par = length(par);
 pi = zeros(1,n_par);
@@ -67,15 +67,14 @@ end
 
 permutation_entropy = -1*sum(pi .*log2(pi));
 
-% finding the stochastic matrix P with elements: equation 3.2
+% finding the stochastic matrix P with elements
 sumOfRows = sum(matriz_adj,2); non_zero_rows = (sumOfRows > 0);
 matriz_P(non_zero_rows,:) = bsxfun(@rdivide, matriz_adj(non_zero_rows,:), sumOfRows(non_zero_rows));
 
-% estimation of the stationary distribution from matriz_adj is - equation
-% 3.4
+% estimation of the stationary distribution from matriz_adj
 matriz_pi = sum(matriz_adj,2)/sum(sum(matriz_adj));
 
-% conditional permutation entropy (h^(CPE)) - equation 3.5
+% conditional permutation entropy (h^(CPE))
 pij = matriz_P.*log2(matriz_P);
 pij(isnan(pij))=0;
 P = -1 .* pi .* sum(pij);
@@ -83,22 +82,19 @@ CP_entropy = sum(P);
 
 % % % local and global node out-link entropy - REBUILD adjacency matrix
 % % % finding the stochastic matrix P with elements WITHOUT self-loops -
-% % % equation 3.6
 matriz_adj2 = matriz_adj - diag(diag(matriz_adj));
 
-% % % finding the NEW stochastic matrix P with elements: equation 3.2 - without
-% self-loops
+% % % finding the NEW stochastic matrix P with elements: without self-loops
 sumOfRows = sum(matriz_adj2,2); non_zero_rows = (sumOfRows > 0);
 matriz_PT(non_zero_rows,:) = bsxfun(@rdivide, matriz_adj2(non_zero_rows,:), sumOfRows(non_zero_rows));
 
 % % shannon entropy of row i gives the local node out-link entropy for node
-% % i: equation 3.7 (shannon_pT) and 3.4 (matriz_pi2 - without self-loogps)
 shannon = matriz_PT .* log2(matriz_PT);
 shannon(isnan(shannon)) = 0;
 shannon_pT = -1 * sum(shannon,2);
 matriz_pi2 = sum(matriz_PT,2)/sum(sum(matriz_PT));
 
-% % global node out-link entropy (GNE) - equation 3.8
+% % global node out-link entropy (GNE)
 gn_entropy = sum(shannon_pT .* matriz_pi2);
 
 
