2eac1b55d4317c2a7ea223fa6dbd98b622375e6f
laurita-santos <73966384+laurita-santos@users.noreply.github.com>
2021-11-03 12:12:10 -0300

    Update ordinal_partition_network.m

--- ordinal_partition_network.m
+++ ordinal_partition_network.m
@@ -6,7 +6,7 @@
 % based on: phylosophical transactions A 375 2017: Multiscale ordinal network
 % analysis of human cardiac dynamics
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% this code is part of paper intituled "Characterisation of neonatal cardiac dynamics using ordinal partition network"
+% this code is part of paper entitled "Characterisation of neonatal cardiac dynamics using ordinal partition network"
 % submitted for Medical & Biological Engineering & Computing - Springer
 
 
